b4ba39c629499037e15d795b506ef1227eea470a
icimod <yzhou65@jhu.edu>
2020-01-12 10:29:12 +0545

    post

--- post_CLIM/3monthly/1.Get3monthstats.sh
+++ post_CLIM/3monthly/1.Get3monthstats.sh
@@ -1,91 +1,70 @@
 #!/bin/sh
 
-#SBATCH --job-name=f_3mon
-#SBATCH --output=./logs/stats_%a.log
-#SBATCH --error=./logs/stats_%a.err
-#SBATCH --time=12:00:00
-#SBATCH --account s1525
-#SBATCH --ntasks=28
-#SBATCH --mail-user=yzhou65@jhu.edu
-#SBATCH --mail-type=ALL
-#SBATCH --array=5-9
-
-export WORKING=/discover/nobackup/projects/grace/
-export LISRUN=/discover/nobackup/projects/grace/Yifan/LISRUN/
-export HINDCAST=$LISRUN/SALDAS/Forecast/hindcast_v1/
-export CLIM=$HINDCAST/hindcast_CLIM/
+mmmdd=oct28
+export WORKING=/data1/LISRUN/Forecast/
+export CLIM=$WORKING/hindcast_CLIM/$mmmdd/
 
 ulimit -v unlimited
 
-module purge
-module load other/nco-4.6.8-gcc-5.3-sp3
-
-vars=( Tair_f_tavg Rainf_f_tavg Evap_tavg SoilMoist_inst Tair_f_tavg_min Tair_f_tavg_max ) 
+vars=( Tair_f_tavg Rainf_f_tavg Evap_tavg SoilMoist_inst ) 
 
 echo "Calculating average and std"
 cd $CLIM/middle/alltime
 
-mm0=`printf %02d $SLURM_ARRAY_TASK_ID`
-mm1=`printf %02d $((10#$mm0+1))`
-mm2=`printf %02d $((10#$mm1+1))`
 
-mkdir -p $CLIM/middle/3monthly/$mm0$mm1$mm2/
-cd $CLIM/middle/3monthly/$mm0$mm1$mm2/
-for var in "${vars[@]}"; do
-    echo "chekcing $mm $var"
-    for yyyy in `seq 2000 2017`; do
-        for e in `seq 0 9`; do
-            {
-            echo "  submitting $yyyy $mm ens$e $var"
-            eval ln -sf $CLIM/middle/alltime/${var}_{$yyyy$mm0..$yyyy$mm2}??.ens$e.nc .
-            find ${var}_$yyyy????.ens$e.nc -xtype l -delete
+for dm in `seq 1 7`; do
+    mm0=`date -d "$mmmdd 2019 +${dm}month" "+%m"`
+    mm1=`printf %02d $((10#$mm0+1))`
+    mm2=`printf %02d $((10#$mm1+1))`
 
-            case ${var: -3} in 
-                "max")
-                ncea -h -y max ${var}_$yyyy????.ens$e.nc -O ${var}_$yyyy$mm0$mm1$mm2.ens$e.nc
-                ;;
-                "min")
-                ncea -h -y min ${var}_$yyyy????.ens$e.nc -O ${var}_$yyyy$mm0$mm1$mm2.ens$e.nc 
-                ;;
-                *)
-                ncea -h ${var}_$yyyy????.ens$e.nc -O ${var}_$yyyy$mm0$mm1$mm2.ens$e.nc
-            esac
-            find . -maxdepth 1 -type l  -name "${var}_$yyyy????.ens$e.nc" -delete
-            echo "  $yyyy $mm ens$e $var done"
-            }&
-            sleep 2s
+    mkdir -p $CLIM/middle/3monthly/$mm0$mm1$mm2/
+    cd $CLIM/middle/3monthly/$mm0$mm1$mm2/
+    for var in "${vars[@]}"; do
+        {
+        echo "chekcing $var for month $mm0"
+        echo "  checking $var $mm0 seperate ensembles"
+        for yyyy in `seq 2000 2018`; do
+            yyyymm0=`date -d "$mmmdd $yyyy +${dm}month" "+%Y%m"`
+            yyyymm1=`date -d "$mmmdd $yyyy +${dm}month" "+%Y%m"`
+            yyyymm2=`date -d "$mmmdd $yyyy +${dm}month" "+%Y%m"`
+            sdd=${sdds[$i]}
+            edd=${edds[$i]}
+            ende=7
+            if [ $yyyy$mm -le 201709 ]; then
+                ende=0
+            fi
+            for e in `seq 0 $ende`; do
+                eval ln -sf $CLIM/middle/alltime/${var}_$yyyymm0??.ens$e.nc .
+                eval ln -sf $CLIM/middle/alltime/${var}_$yyyymm1??.ens$e.nc .
+                eval ln -sf $CLIM/middle/alltime/${var}_$yyyymm2??.ens$e.nc .
+                find ${var}_$yyyymm0??.ens$e.nc -xtype l -delete
+                find ${var}_$yyyymm1??.ens$e.nc -xtype l -delete
+                find ${var}_$yyyymm2??.ens$e.nc -xtype l -delete
+
+                ncea -h ${var}_20??????.ens$e.nc -O ${var}_$yyyy$mm0$mm1$mm2.ens$e.nc
+                find . -maxdepth 1 -type l  -name "${var}_$yyyymm0??.ens$e.nc" -delete
+                find . -maxdepth 1 -type l  -name "${var}_$yyyymm1??.ens$e.nc" -delete
+                find . -maxdepth 1 -type l  -name "${var}_$yyyymm2??.ens$e.nc" -delete
+            done
         done
+        echo "  calculating $var $mm0 stats"
+        ncecat -h ${var}_20??$mm0$mm1$mm2.ens?.nc -O $var.$mm0$mm1$mm2.alltime.nc
+        rm ${var}_20??$mm0$mm1$mm2.ens?.nc
+        ncwa -h -a record $var.$mm0$mm1$mm2.alltime.nc -O $var.$mm0$mm1$mm2.avg.nc
+        ncdiff -h $var.$mm0$mm1$mm2.alltime.nc $var.$mm0$mm1$mm2.avg.nc -O $var.$mm0$mm1$mm2.diff.nc
+        rm $var.$mm0$mm1$mm2.alltime.nc
+
+        ncra -h -y rmssdn $var.$mm0$mm1$mm2.diff.nc -O $var.$mm0$mm1$mm2.std.1d.nc
+        rm $var.$mm0$mm1$mm2.diff.nc
+        ncwa -h -a record $var.$mm0$mm1$mm2.std.1d.nc -O $var.$mm0$mm1$mm2.std.nc
+        rm $var.$mm0$mm1$mm2.std.1d.nc
+
+        mkdir -p $CLIM/3monthly
+        mv $var.$mm0$mm1$mm2.???.nc $CLIM/3monthly/
+        echo "$var for month $mm0 done"
+        } & 
+        sleep 5s
     done
-    wait
-    echo "all $mm $var done"
-    {
-    echo "$mm $var avg & std"
-    ncecat -h ${var}_20??$mm0$mm1$mm2.ens?.nc -O $var.$mm0$mm1$mm2.alltime.nc
-    rm ${var}_20??$mm0$mm1$mm2.ens?.nc
-    ncwa -h -a record $var.$mm0$mm1$mm2.alltime.nc -O $var.$mm0$mm1$mm2.avg.nc
-    ncdiff -h $var.$mm0$mm1$mm2.alltime.nc $var.$mm0$mm1$mm2.avg.nc -O $var.$mm0$mm1$mm2.diff.nc
-    rm $var.$mm0$mm1$mm2.alltime.nc
-
-    ncra -h -y rmssdn $var.$mm0$mm1$mm2.diff.nc -O $var.$mm0$mm1$mm2.std.1d.nc
-    rm $var.$mm0$mm1$mm2.diff.nc
-    ncwa -h -a record $var.$mm0$mm1$mm2.std.1d.nc -O $var.$mm0$mm1$mm2.std.nc
-    rm $var.$mm0$mm1$mm2.std.1d.nc
-
-    if [ "$var" == "Tair_f_tavg" ];
-    then
-        cp $var.$mm0$mm1$mm2.avg.nc 3monthly.$mm0$mm1$mm2.avg.nc
-        cp $var.$mm0$mm1$mm2.std.nc 3monthly.$mm0$mm1$mm2.std.nc
-    else
-        ncks -h -A $var.$mm0$mm1$mm2.avg.nc 3monthly.$mm0$mm1$mm2.avg.nc
-        ncks -h -A $var.$mm0$mm1$mm2.std.nc 3monthly.$mm0$mm1$mm2.std.nc
-    fi
-    echo "$mm $var avg & std done!!!"
-    }&
-    sleep 45s
 done
 wait
-mkdir -p $CLIM/3monthly
-mv 3monthly.$mm0$mm1$mm2.avg.nc $CLIM/3monthly/
-mv 3monthly.$mm0$mm1$mm2.std.nc $CLIM/3monthly/
-echo "$mm $var done"
-
+echo "All done!"
